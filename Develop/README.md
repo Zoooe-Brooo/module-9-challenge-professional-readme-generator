0fb6baa44e995d95123cba08d5c0776615f73630
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-07-31 23:07:24 +0930

    Update index.js, generateMarkdown.js, package.json and Create README

---
+++ Develop/README.md
@@ -0,0 +1,28 @@
+# Professional README Generator ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+## Description
+A command-line application that generates a professional README.md file from user's input.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+Node.js, npm and code editor need to be installed in advance, then init the package.json by runing: 'npm init -y', in addition, inquirer package also need to be installed with npm in advance. Last, application's JS files need to be clone to the local machine.
+## Usage
+Open the CLI of the corresponding directory, then run: 'node index'.
+## License
+This project is licensed under the MIT license.
+
+License information can be found [here](https://choosealicense.com/licenses/mit/).
+## Contributing
+Anyone has the knowledge and willing to contribute to this application is welcome.
+## Tests
+N/A
+## Questions
+If you have any questions, please feel free to contact me:
+
+GitHub: https://github.com/Zoooe-Brooo
+
+Email: zhongwenli.zoe@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return `License information can be found [here](https://choosealicense.com/licenses/${license.toLowerCase()}/).`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${renderLicenseSection(data.license)}
 
+${renderLicenseLink(data.license)}
+## Contributing
+${data.contribution}
+## Tests
+${data.test}
+## Questions
+If you have any questions, please feel free to contact me:
+
+GitHub: https://github.com/${data.github}
+
+Email: ${data.email}
 `;
 }
 
